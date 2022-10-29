97e09b1a40ae08f64e2195aa2f2dbdad710c69a3
nhat2012782 <gonbbb1@gmail.com>
2022-10-29 15:51:59 +0700

    fix register

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,7 @@ import authController from "../controllers/authController.js";
 import middlewareController from "../controllers/middlewareController.js";
 
 const router = express.Router();
-router.post(
-  "/register",
-  middlewareController.verifyToken,
-  authController.registerUser
-);
+router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.post("/refresh", authController.requestRefreshToken);
 router.post(
